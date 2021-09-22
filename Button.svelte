3e294f7196b711ba85c2f33336922af0d6edba35
Igor Bedesqui <igorbedesqui@gmail.com>
2021-09-21 21:17:34 -0300

    Initial commit

---
+++ Button.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { css } from "@stitches/core";
+	let count = 0;
+
+	function handleClick() {
+	  count += 1;
+	}
+
+	const button = css({
+	  background: "gray",
+	  color: "white",
+	  border: "none",
+	  padding: "8px 12px",
+	  borderRadius: "2px",
+
+	  "@media screen and (prefers-reduced-motion: no-preference)": {
+	    transitionTimingFunction: "cubic-bezier(0.2, 0, 0.38, 0.9)",
+	    transitionDuration: "70ms"
+	  },
+
+	  "&:hover": {
+	    background: "black"
+	  }
+	});
+</script>
+
+<button class={button()} on:click={handleClick}>
+  Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
